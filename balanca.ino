856bec720be2577c99c3983fb9e864b56dfac810
Denilson Santos <89925492+denilsondossantos@users.noreply.github.com>
2022-05-05 08:14:50 -0300

    Otimização do código
    Função "getPesoPote()", passa a retornar o peso da balança.

--- balanca.ino
+++ balanca.ino
@@ -1,62 +1,14 @@
-
-HX711 scale;
-
-float kg = 0;
-float pesoLido = 0;
-float taraPeso = 0;
-
-void configBalanca()
-{
-    scale.begin(DT_PIN, SCK_PIN);
-    delay(1000);
-    Serial.begin(9600);
-    Serial.println("Serial iniciada:");
-    taraPeso = calibra();
+ 
+void configBalancaPote() {
+  Serial.println(escala.read());   // Aguada até o dispositivo estar pronto
+  escala.set_scale(913474.57);     // Substituir o valor encontrado para escala
+  escala.tare(20);                // O peso é chamado de Tare.
 }
-
-void rodaBalanca()
-{   
-peso();
+ 
+float getPesoPote() {
+  float p = 0;
+  p = escala.get_units(20);
+  //Serial.print("A última leitura do peso lido foi: ");
+  //Serial.print(pesoPote);
+  return p;
 }
-
-
-float calibra(){
-  float maiorValor = 0;
-  float  vetor [16];
-  float tara = 0;
-
-  Serial.println("Calibrando balança...");
-  maiorValor  = scale.read_average();
-    for(int x = 0; x <= 15; x++){
-      vetor[x] = scale.read_average();
-      if(maiorValor < scale.read_average()){
-        maiorValor = scale.read_average();
-        }
-    }
-
-    
-tara  = maiorValor + 300;
-return tara;
-
-}
-
-void peso(){
-    pesoLido  = scale.get_units()/1000;
-    if(scale.read_average() < taraPeso){
-    kg = 0;
-    Serial.print("KG: ");
-    Serial.println(kg);
-    
-    }
-    else if (pesoLido < 1000 && pesoLido > 10) {
-      Serial.print("Gramas: ");
-      Serial.println(pesoLido);
-      delay(1000);
-      }
-
-      else{
-      float pesoLido = scale.get_units() / 1000;
-      Serial.println(pesoLido);
-      delay(1000);
-      }
-    }
