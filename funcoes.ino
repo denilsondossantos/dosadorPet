0d773ef0688231a32f593d7da8fa319767475074
Denilson Santos <89925492+denilsondossantos@users.noreply.github.com>
2022-05-16 13:45:47 -0300

    Função funcionando
    Adicionado função do motor

--- funcoes.ino
+++ funcoes.ino
@@ -1,35 +1,33 @@
-/*
-#define motor 22
-
-
-*/
-
-
-
 
 /*
-agenda(int hora, int minuto){ 
-  for(int i = 0; i<= 3; i++){
-    if(hora == h[i] && minuto == m[i]){
-      acionaMotor(pesoGramas);
-      }
-  }      
-}
+Função que completa a ração no pote do pet 
 */
-
-
-acionaMotor(void * parameter){
-  while(true){  
+void acionaMotor(void * parameter){
+  while(true){
+    Serial.println("Entrei na task motor");  
     printaData(); //atualiza data da variavel global
+    Serial.print("hora:");  
+    Serial.print(dataAgora[0]);  
+    Serial.print(" "); 
+    Serial.print("minuto:");   
+    Serial.print(dataAgora[1]);
+    Serial.println("\n");
 
-      for(int i = 0; i <= 2; i++){
-        if(horaAgora[0] == agenda[i].hora && horaAgora[1] == agenda[i].minuto){    
-          while(100 <= 50){
-            digitalWrite(motor, HIGH);
+      for(int i = 0; i <= tamanhoAgenda; i++){
+        if(dataAgora[0] == agenda[i].hora && dataAgora[1] == agenda[i].minuto){ 
+          Serial.println("ENTREI NA FUNÇÃO DO MOTOR");   
+          if(getPesoPote() >= 0.100){
+            Serial.println("Pote cheio");  
+          }else{
+                do
+                  {
+                   digitalWrite(motor, HIGH);
+                   vTaskDelay(100);
+              }while (getPesoPote()<= 0.100);
+            }            
+            digitalWrite(motor, LOW);
           }
-          digitalWrite(motor, LOW);
         }
+        vTaskDelay(10000);
       }      
-  xTaskDelay(10000);
   }   
-}
