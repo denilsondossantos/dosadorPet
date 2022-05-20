69e108b99346447e9a2139064544c59c0048442e
Denilson Santos <89925492+denilsondossantos@users.noreply.github.com>
2022-05-20 15:37:49 -0300

    Passa a considerar o peso do agendamento

--- funcoes.ino
+++ funcoes.ino
@@ -4,30 +4,31 @@ Função que completa a ração no pote do pet
 */
 void acionaMotor(void * parameter){
   while(true){
-    Serial.println("Entrei na task motor");  
     printaData(); //atualiza data da variavel global
-    Serial.print("hora:");  
-    Serial.print(dataAgora[0]);  
-    Serial.print(" "); 
-    Serial.print("minuto:");   
-    Serial.print(dataAgora[1]);
-    Serial.println("\n");
+      //    Serial.println("Entrei na task motor");  
+      //    Serial.print("hora:");  
+      //    Serial.print(dataAgora[0]);  
+      //    Serial.print(" "); 
+      //    Serial.print("minuto:");   
+      //    Serial.print(dataAgora[1]);
+      //    Serial.println("\n");
 
-      for(int i = 0; i <= tamanhoAgenda; i++){
+    for(int i = 0; i <= tamanhoAgenda; i++){
         if(dataAgora[0] == agenda[i].hora && dataAgora[1] == agenda[i].minuto){ 
           Serial.println("ENTREI NA FUNÇÃO DO MOTOR");   
-          if(getPesoPote() >= 0.100){
+          if(getPesoPote() >= agenda[i].peso/1000.0){
             Serial.println("Pote cheio");  
           }else{
                 do
                   {
                    digitalWrite(motor, HIGH);
                    vTaskDelay(100);
-              }while (getPesoPote()<= 0.100);
+              }while (getPesoPote()<= agenda[i].peso/1000.0);
             }            
             digitalWrite(motor, LOW);
           }
-        }
-        vTaskDelay(10000);
-      }      
+    }
+    vTaskDelay(10000);
   }   
+}      
+  
