213708c441a8d3f20d942fe1a7425b70d4dad732
Denilson Santos <89925492+denilsondossantos@users.noreply.github.com>
2022-06-03 11:09:04 -0300

    Adicionado segunda balança

--- balanca.ino
+++ balanca.ino
@@ -1,6 +1,6 @@
  
 void configBalancaPote() {
-  Serial.println(escala.read());   // Aguada até o dispositivo estar pronto
+  Serial.println(escala.read());   // Aguarda até o dispositivo estar pronto
   escala.set_scale(913474.57);     // Substituir o valor encontrado para escala
   escala.tare(20);                // O peso é chamado de Tare.
 }
@@ -8,15 +8,30 @@ void configBalancaPote() {
 float getPesoPote() {
   float p = 0;
   p = escala.get_units(20);
-  //Serial.print("A última leitura do peso lido foi: ");
-  //Serial.print(pesoPote);
-  if(p<0){
-    return 0;
+    if(p<0){
+      return 0;
     }
   else{
-    String b = String(p,3);
-    float  c = b.toFloat();
-    return c;    
+    Serial.println(String(p,3).toFloat());  
+    return String(p,3).toFloat();   
+  }  
+}
+
+//-------------------------------------------------------------------
+void configBalancaMaior() {
+  Serial.println(escala1.read());
+  escala1.set_scale(22.560);      // Substituir o valor encontrado para escala
+  escala1.tare(10);               // O peso é chamado de Tare.
+}
+ 
+float getPesoMaior() {
+ float peso = 0;
+ peso = escala1.get_units(20)/1000;
+  if(peso < 0 ){
+      return 0;
     }
-  
+  else{
+    Serial.println(String(peso,3).toFloat());  
+    return String(peso,3).toFloat(); 
+  }
 }
