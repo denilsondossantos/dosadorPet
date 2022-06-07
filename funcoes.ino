849afe728d12e6625620a01d63fc8b8e283a24e2
Denilson Santos <89925492+denilsondossantos@users.noreply.github.com>
2022-06-07 16:48:14 -0300

    variavel block adicionada
    Alteração para evitar conflito das tasks

--- funcoes.ino
+++ funcoes.ino
@@ -5,9 +5,13 @@ void acionaMotor(void * parameter){
   while(true){
     printaData(); //atualiza data da variavel global
 
-    for(int i = 0; i <= tamanhoAgenda; i++){
-        if(dataAgora[0] == agenda[i].hora && dataAgora[1] == agenda[i].minuto){ 
-          Serial.println("ENTREI NA FUNÇÃO DO MOTOR");   
+    for(int i = 0; i <= 3; i++)
+    {
+        if(dataAgora[0] == agenda[i].hora && dataAgora[1] == agenda[i].minuto)
+        { 
+          block = true;   //debug 
+          vTaskDelay(100);
+          Serial.println("ENTREI NA FUNÇÃO DO MOTOR");
           if(getPesoPote() >= agenda[i].peso){
             Serial.println("Pote cheio");  
           }else{
@@ -15,12 +19,13 @@ void acionaMotor(void * parameter){
                   {
                    digitalWrite(motor, HIGH);
                    vTaskDelay(100);
-              }while (getPesoPote()<= agenda[i].peso);
-            }            
+                  }while (getPesoPote()<= agenda[i].peso);
+                }            
             digitalWrite(motor, LOW);
-          }
+         }
+         block = false;
     }
+    //block = false;
     vTaskDelay(10000);
   }   
-}      
-  
+}    
