a465ed4a0964e7a2189abdd9e4a0b9b275fb2c62
Denilson Santos <89925492+denilsondossantos@users.noreply.github.com>
2022-06-07 13:15:40 -0300

    Correção de bugs
    Não divide por 1000 ao comparar com o peso da balança.

--- funcoes.ino
+++ funcoes.ino
@@ -1,29 +1,21 @@
-
 /*
 Função que completa a ração no pote do pet 
 */
 void acionaMotor(void * parameter){
   while(true){
     printaData(); //atualiza data da variavel global
-      //    Serial.println("Entrei na task motor");  
-      //    Serial.print("hora:");  
-      //    Serial.print(dataAgora[0]);  
-      //    Serial.print(" "); 
-      //    Serial.print("minuto:");   
-      //    Serial.print(dataAgora[1]);
-      //    Serial.println("\n");
 
     for(int i = 0; i <= tamanhoAgenda; i++){
         if(dataAgora[0] == agenda[i].hora && dataAgora[1] == agenda[i].minuto){ 
           Serial.println("ENTREI NA FUNÇÃO DO MOTOR");   
-          if(getPesoPote() >= agenda[i].peso/1000.0){
+          if(getPesoPote() >= agenda[i].peso){
             Serial.println("Pote cheio");  
           }else{
                 do
                   {
                    digitalWrite(motor, HIGH);
                    vTaskDelay(100);
-              }while (getPesoPote()<= agenda[i].peso/1000.0);
+              }while (getPesoPote()<= agenda[i].peso);
             }            
             digitalWrite(motor, LOW);
           }
